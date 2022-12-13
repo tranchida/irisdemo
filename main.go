510581c090debfa336c95619ddf8b9fe9ea18a1e
Giampaolo Tranchida <giampaolo@tranchida.ch>
2022-12-13 23:09:13 +0100

    test

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+	"time"
 
 	"github.com/kataras/golog"
 	"github.com/kataras/iris/v12"
@@ -16,9 +17,122 @@ import (
 const uri = "mongodb+srv://giampa0l0:PkZ66tL$X9@cluster0.7xoftco.mongodb.net/?retryWrites=true&w=majority"
 
 type Car struct {
-	Id    primitive.ObjectID `bson:"_id,omitempty"`
-	Model string             `bson:"model,omitempty"`
-	Color string             `bson:"color,omitempty"`
+	ID                   string `bson:"_id"`
+	ListingURL           string `bson:"listing_url"`
+	Name                 string `bson:"name"`
+	Summary              string `bson:"summary"`
+	Space                string `bson:"space"`
+	Description          string `bson:"description"`
+	NeighborhoodOverview string `bson:"neighborhood_overview"`
+	Notes                string `bson:"notes"`
+	Transit              string `bson:"transit"`
+	Access               string `bson:"access"`
+	Interaction          string `bson:"interaction"`
+	HouseRules           string `bson:"house_rules"`
+	PropertyType         string `bson:"property_type"`
+	RoomType             string `bson:"room_type"`
+	BedType              string `bson:"bed_type"`
+	MinimumNights        string `bson:"minimum_nights"`
+	MaximumNights        string `bson:"maximum_nights"`
+	CancellationPolicy   string `bson:"cancellation_policy"`
+	LastScraped          struct {
+		Date time.Time `bson:"$date"`
+	} `bson:"last_scraped"`
+	CalendarLastScraped struct {
+		Date time.Time `bson:"$date"`
+	} `bson:"calendar_last_scraped"`
+	FirstReview struct {
+		Date time.Time `bson:"$date"`
+	} `bson:"first_review"`
+	LastReview struct {
+		Date time.Time `bson:"$date"`
+	} `bson:"last_review"`
+	Accommodates    int      `bson:"accommodates"`
+	Bedrooms        int      `bson:"bedrooms"`
+	Beds            int      `bson:"beds"`
+	NumberOfReviews int      `bson:"number_of_reviews"`
+	Bathrooms       float64  `bson:"bathrooms"`
+	Amenities       []string `bson:"amenities"`
+	Price           struct {
+		NumberDecimal string `bson:"$numberDecimal"`
+	} `bson:"price"`
+	WeeklyPrice struct {
+		NumberDecimal string `bson:"$numberDecimal"`
+	} `bson:"weekly_price"`
+	MonthlyPrice struct {
+		NumberDecimal string `bson:"$numberDecimal"`
+	} `bson:"monthly_price"`
+	CleaningFee struct {
+		NumberDecimal string `bson:"$numberDecimal"`
+	} `bson:"cleaning_fee"`
+	ExtraPeople struct {
+		NumberDecimal string `bson:"$numberDecimal"`
+	} `bson:"extra_people"`
+	GuestsIncluded struct {
+		NumberDecimal string `bson:"$numberDecimal"`
+	} `bson:"guests_included"`
+	Images struct {
+		ThumbnailURL string `bson:"thumbnail_url"`
+		MediumURL    string `bson:"medium_url"`
+		PictureURL   string `bson:"picture_url"`
+		XlPictureURL string `bson:"xl_picture_url"`
+	} `bson:"images"`
+	Host struct {
+		HostID                 string   `bson:"host_id"`
+		HostURL                string   `bson:"host_url"`
+		HostName               string   `bson:"host_name"`
+		HostLocation           string   `bson:"host_location"`
+		HostAbout              string   `bson:"host_about"`
+		HostResponseTime       string   `bson:"host_response_time"`
+		HostThumbnailURL       string   `bson:"host_thumbnail_url"`
+		HostPictureURL         string   `bson:"host_picture_url"`
+		HostNeighbourhood      string   `bson:"host_neighbourhood"`
+		HostResponseRate       int      `bson:"host_response_rate"`
+		HostIsSuperhost        bool     `bson:"host_is_superhost"`
+		HostHasProfilePic      bool     `bson:"host_has_profile_pic"`
+		HostIdentityVerified   bool     `bson:"host_identity_verified"`
+		HostListingsCount      int      `bson:"host_listings_count"`
+		HostTotalListingsCount int      `bson:"host_total_listings_count"`
+		HostVerifications      []string `bson:"host_verifications"`
+	} `bson:"host"`
+	Address struct {
+		Street         string `bson:"street"`
+		Suburb         string `bson:"suburb"`
+		GovernmentArea string `bson:"government_area"`
+		Market         string `bson:"market"`
+		Country        string `bson:"country"`
+		CountryCode    string `bson:"country_code"`
+		Location       struct {
+			Type            string    `bson:"type"`
+			Coordinates     []float64 `bson:"coordinates"`
+			IsLocationExact bool      `bson:"is_location_exact"`
+		} `bson:"location"`
+	} `bson:"address"`
+	Availability struct {
+		Availability30  int `bson:"availability_30"`
+		Availability60  int `bson:"availability_60"`
+		Availability90  int `bson:"availability_90"`
+		Availability365 int `bson:"availability_365"`
+	} `bson:"availability"`
+	ReviewScores struct {
+		ReviewScoresAccuracy      int `bson:"review_scores_accuracy"`
+		ReviewScoresCleanliness   int `bson:"review_scores_cleanliness"`
+		ReviewScoresCheckin       int `bson:"review_scores_checkin"`
+		ReviewScoresCommunication int `bson:"review_scores_communication"`
+		ReviewScoresLocation      int `bson:"review_scores_location"`
+		ReviewScoresValue         int `bson:"review_scores_value"`
+		ReviewScoresRating        int `bson:"review_scores_rating"`
+	} `bson:"review_scores"`
+	Reviews []struct {
+		ID   string `bson:"_id"`
+		Date struct {
+			Date time.Time `bson:"$date"`
+		} `bson:"date"`
+		ListingID    string `bson:"listing_id"`
+		ReviewerID   string `bson:"reviewer_id"`
+		ReviewerName string `bson:"reviewer_name"`
+		Comments     string `bson:"comments"`
+	} `bson:"reviews"`
 }
 
 var carCollection *mongo.Collection
