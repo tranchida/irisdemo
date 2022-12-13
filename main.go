b75923c0d8ed42230dec7de4bc902b642aa32f42
Giampaolo Tranchida <giampaolo@tranchida.ch>
2022-12-13 06:35:31 +0100

    improve

--- main.go
+++ main.go
@@ -1,17 +1,124 @@
 package main
 
-import "github.com/kataras/iris/v12"
+import (
+	"context"
+	"log"
+
+	"github.com/kataras/golog"
+	"github.com/kataras/iris/v12"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// const uri = "mongodb://localhost:27017"
+const uri = "mongodb+srv://giampa0l0:PkZ66tL$X9@cluster0.7xoftco.mongodb.net/?retryWrites=true&w=majority"
+
+type Car struct {
+	Id    primitive.ObjectID `bson:"_id,omitempty"`
+	Model string             `bson:"model,omitempty"`
+	Color string             `bson:"color,omitempty"`
+}
+
+var carCollection *mongo.Collection
 
 func main() {
 
+	var err error
+	carCollection, err = connectMongo()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	app := createApp()
+	app.Listen(":8080")
+
+}
+
+func connectMongo() (*mongo.Collection, error) {
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	car := client.Database("viacar").Collection("car")
+
+	if err != nil {
+		return nil, err
+	}
+
+	return car, nil
+}
+
+func createApp() *iris.Application {
+
 	app := iris.New()
 
-	app.Get("/", func(ctx iris.Context) {
+	//f, _ := os.Create("iris.log")
+	//app.Logger().SetOutput(f)
 
-		ctx.WriteString("Hello world with air")
+	carParty := app.Party("/car")
 
-	})
+	carParty.Get("/", listAllCars)
 
-	app.Listen(":8080")
+	carParty.Get("/{id}", findCarById)
+
+	carParty.Post("/", addCar)
+
+	return app
+}
+
+func listAllCars(ctx iris.Context) {
+
+	cursor, err := carCollection.Find(context.TODO(), bson.D{})
+	if err != nil {
+		ctx.StopWithError(iris.StatusInternalServerError, err)
+		return
+	}
+
+	var cars []Car
+
+	if err = cursor.All(context.TODO(), &cars); err != nil {
+		ctx.StopWithError(iris.StatusInternalServerError, err)
+		return
+	}
+
+	ctx.JSON(cars)
+}
+
+func findCarById(ctx iris.Context) {
+
+	id := ctx.Params().Get("id")
+	docId, err := primitive.ObjectIDFromHex(id)
+	println(id)
+	if err != nil {
+		ctx.StopWithError(iris.StatusBadRequest, err)
+		return
+	}
+
+	var car Car
+	err = carCollection.FindOne(context.TODO(), bson.M{"_id": docId}).Decode(&car)
+	if err != nil {
+		ctx.StopWithError(iris.StatusInternalServerError, err)
+		return
+	}
+	ctx.JSON(car)
+
+}
+
+func addCar(ctx iris.Context) {
+
+	var car Car
+	err := ctx.ReadBody(&car)
+	if err != nil {
+		ctx.StopWithError(iris.StatusInternalServerError, err)
+		return
+	}
+
+	result, err := carCollection.InsertOne(context.TODO(), car)
+	if err != nil {
+		ctx.StopWithError(iris.StatusInternalServerError, err)
+		return
+	}
+
+	ctx.Application().Logger().Log(golog.InfoLevel, result.InsertedID)
 
 }
